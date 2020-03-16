1bb76e765a1f0f041f1798ae32e9bf88fe25ed5c
Tanya Klintsevich <tanya-klin@list.ru>
2020-03-16 03:03:21 +0300

    Practice 4,5

---
+++ Lesson-8/4,5.js
@@ -0,0 +1,34 @@
+function Cat(name) {
+    var foodAmount = 50;
+    function formatFoodAmount() {
+        return foodAmount + ' гр.';
+    }
+    this.dailyNorm = function(amount) {
+        if (!arguments.length) return formatFoodAmount();
+        if (amount < 50 || amount > 500) {
+           return 'Недопустимое количество корма.';
+        }
+        foodAmount = amount;
+    };
+    this.name = name;
+    this.feed = function() {
+        console.log('Насыпаем в миску ' + this.dailyNorm() + ' корма.');
+    };
+}
+function Cat(name) {
+	Animal.call(this);
+	var animalFeed = this.feed;
+	this.feed = function () {
+		animalFeed.call(this);
+		console.log('Кот доволен ^_^');
+		return this;
+	}
+
+	this.stroke = function() {
+		console.log('Гладим кота.');
+		return this;
+	}
+}
+
+var cat = new Cat('Solli');
+cat.stroke().feed();
