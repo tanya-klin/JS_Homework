cd66f5e5cd642ce082bed391fc45f4fa9abbe118
Tanya Klintsevich <tanya-klin@list.ru>
2020-03-30 09:18:40 +0300

    Homework

---
+++ Lesson-12/practice 1-4.js
@@ -0,0 +1,48 @@
+// 1
+
+function obj(i) {
+    return i.map(function (i) {
+        return { name:i };
+    });
+}
+
+obj(['Tanya','Anya','Kate']);
+
+// 2
+
+var arr = ['00', '13', '24'];
+
+function getTime(value) {
+    return value.reduce(function (string, value) {
+        return string + ' : ' + value;
+    }, 'Текущее время');
+}
+
+getTime(arr);
+
+// 3
+
+function howMuchVowel(string) {
+    var vowels = ['а', 'о', 'и', 'е', 'ё', 'э', 'ы', 'у', 'ю', 'я'];
+
+    var sentence = string.toLowerCase().split('');
+
+    var countVowels = sentence.reduce(function(a,b) {
+        if (a.indexOf (value) != -1) {
+            a++;
+        }
+        a;
+    }, 0);
+    return countVowels;
+}
+
+getvowelsAmount('Эти привычки могут привести к неожиданному поведению программы.');
+
+//4
+
+function countLetters(string) {
+    var newSentences = string.split(/[\.\!\?]/);
+
+    var filteredArrOfSentences = arrOfSentences.filter(function(elem) {
+        return elem;
+    });
